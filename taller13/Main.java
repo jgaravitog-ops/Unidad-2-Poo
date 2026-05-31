53f5c5c94bc68a57b810c744e4cd2890ddf6befe
JgaravitoG <jgaravitog@unicartagena.edu.co>
2026-05-31 01:32:59 -0500

    taller13 ejercicio2

--- taller13/Main.java
+++ taller13/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
         System.out.println("-----------------");
         //ejercicio 2
-        
+        Pato pato1 = new Pato("Domestico");
+        pato1.volar();
+        pato1.nadar();
     }
 }
---
+++ taller13/Pato.java
@@ -0,0 +1,22 @@
+package taller13;
+
+import taller12.Nadador;
+import taller12.Volador;
+
+public class Pato implements Volador,Nadador{
+    String especie;
+
+    public Pato(String especie){
+        this.especie = especie;
+    }
+
+    @Override
+    public void nadar() {
+        System.out.println("Pato "+especie + " esta nadanado");
+    }
+    @Override
+    public void volar() {
+        System.out.println("Pato "+especie+" esta volando");
+    }
+
+}
